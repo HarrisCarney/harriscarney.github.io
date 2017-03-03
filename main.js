35d69077a714e1b4d2dc37a9650d0113e2a91da9
Harris Carney <harriskirklandcarney@gmail.com>
2017-03-02 20:12:41 -0600

    ill never decide on a design

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var menu = document.getElementsByClassName('navigation--mobile')[0];
+var links = document.getElementsByClassName('navigation--link');
 var active = false;
 
 menu.onclick = function() {
 	if(!active) {
+		for(var i = 0; i < links.length; i++) {
+			links[i].onclick = function() {
+				document.getElementsByClassName('navigation')[0].classList.remove('active');
+			}
+		}
 		document.getElementsByClassName('navigation')[0].classList.add('active');
 	} else {
 		document.getElementsByClassName('navigation')[0].classList.remove('active');
