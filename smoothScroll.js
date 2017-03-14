992fe75c07e64075b264b39eaa4c0f7d9ac81611
Harris Carney <harriskirklandcarney@gmail.com>
2017-03-13 22:27:34 -0500

    Well my scrollScrolling code didn’t actually make it on that commit…….

---
+++ smoothScroll.js
@@ -0,0 +1,24 @@
+var offset;
+
+function smoothScroll() {
+  var start    = self.pageYOffset;
+  var end      = offset;
+
+  var distance = Math.round(start > end ? start - end : end - start);
+  var speed    = distance / 10;
+  var step     = start > end ? start - speed : start + speed;
+
+  console.log(distance + ', ' + speed + ', ' + step);
+
+  if(distance > 0 && speed < 1) {
+    step = start > end ? start - 1 : start + 1;
+  }
+
+  if(start !== end) {
+    window.scrollTo(0, step);
+  }
+
+  if(distance > 0 && (window.innerHeight + window.scrollY) < (document.body.offsetHeight + 60)) {
+    requestAnimationFrame(smoothScroll);
+  }
+}
